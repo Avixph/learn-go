ce0c7147965cbbad7a4e53fce253b3d828602dbd
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2023-03-01 13:48:12 -0500

    completed ninja level 4

--- courses/learn-to-code-in-go/exercises/ninja-level-4/excercise-h/main.go
+++ courses/learn-to-code-in-go/exercises/ninja-level-4/excercise-h/main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	jb := []string{"Shaken, not stirred", "Martinis", "Woman"}
+	mm := []string{"James Bond", "Literature", "Computer Science"}
+	dn := []string{"Being evil", "Ice cream", "unsets"}
 	jbCharacters := map[string][]string{
-		"bond_james":      []string{"Shaken, not stirred", "Martinis", "Woman"},
-		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
-		"no_dr":           []string{"Being evil", "Ice cream", "unsets"},
+		"bond_james":      jb,
+		"moneypenny_miss": mm,
+		"no_dr":           dn,
 	}
 
 	for i, characters := range jbCharacters {
--- courses/learn-to-code-in-go/exercises/ninja-level-4/excercise-i/main.go
+++ courses/learn-to-code-in-go/exercises/ninja-level-4/excercise-i/main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	jb := []string{"Shaken, not stirred", "Martinis", "Woman"}
+	mm := []string{"James Bond", "Literature", "Computer Science"}
+	dn := []string{"Being evil", "Ice cream", "unsets"}
 	jbCharacters := map[string][]string{
-		"bond_james":      []string{"Shaken, not stirred", "Martinis", "Woman"},
-		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
-		"no_dr":           []string{"Being evil", "Ice cream", "unsets"},
+		"bond_james":      jb,
+		"moneypenny_miss": mm,
+		"no_dr":           dn,
 	}
 
 	jbCharacters["fleming_ian"] = []string{"steak", "cigars", "espionage"}
