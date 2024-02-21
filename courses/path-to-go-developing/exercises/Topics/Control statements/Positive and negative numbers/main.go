5229290c1ddc5bbf32be0b013f0d5a5218683ed2
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2024-02-21 14:53:20 -0500

    worked on pointer and conditional excercises

--- courses/path-to-go-developing/exercises/Topics/Control statements/Positive and negative numbers/main.go
+++ courses/path-to-go-developing/exercises/Topics/Control statements/Positive and negative numbers/main.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main() {
 	var number int
-    fmt.Scanf("%d", &number)
+	fmt.Scanf("%d", &number)
 
-    // Write your code here.
+	switch {
+	case number < 0:
+		fmt.Println("Negative!")
+	case number > 0:
+		fmt.Println("Positive!")
+	default:
+		fmt.Println("Zero!")
+	}
 }
