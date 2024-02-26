863936a3b7c2f902a0077a9a280fd58e5d73af51
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2024-02-26 16:33:27 -0500

    worked on maps and package excercises

--- courses/path-to-go-developing/exercises/Topics/Operations with maps/Best football players of 2021/main.go
+++ courses/path-to-go-developing/exercises/Topics/Operations with maps/Best football players of 2021/main.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-    // do not change the contents of the nominees map!
-	nominees := map[string]string {
-		"Lionel Messi": "Paris Sant-Germain",
+	// do not change the contents of the nominees map!
+	nominees := map[string]string{
+		"Lionel Messi":       "Paris Sant-Germain",
 		"Robert Lewandowski": "Bayern Munich",
-		"Jorginho": "Chelsea",
-		"Cristiano Ronaldo": "Manchester United",
-        "Karim Benzema": "Real Madrid",
+		"Jorginho":           "Chelsea",
+		"Cristiano Ronaldo":  "Manchester United",
+		"Karim Benzema":      "Real Madrid",
 	}
 
-	fmt.Println(?) // print the length of the nominees map here
+	fmt.Println(len(nominees)) // print the length of the nominees map here
 }
---
+++ courses/path-to-go-developing/exercises/Topics/Packages/Fix the broken code/main.go
@@ -0,0 +1,13 @@
+pkg main
+
+import "format"
+
+func init() {
+    fmt.Println(emoji())
+}
+
+// DO NOT change the code of the emoji() function! look out for other errors!
+func emoji() string {
+	// return an emoji
+	return "👨‍💻" + " 🏆 Well done! 🏆 " + "👩‍💻"
+}
