1c4d8f1aece6bc62e7db2591e69c5d49f148c227
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2024-02-29 12:42:13 -0500

    worked on interface excercises

---
+++ courses/path-to-go-developing/exercises/Topics/Comments/Go! Golang comments/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go!")
+	// fmt.Println("1")
+	// fmt.Println("@")
+	// fmt.Println("Hello, ")
+
+	fmt.Println("Golang!!")
+	// fmt.Println("World")
+
+	fmt.Println("Go go go!!!")
+}
