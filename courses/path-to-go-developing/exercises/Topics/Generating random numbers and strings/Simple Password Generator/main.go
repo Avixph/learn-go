d06fa2b88e1f36901b6743ce2c0c708a46d95d6a
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2024-02-29 20:05:03 -0500

    worked on rand package excercises

--- courses/path-to-go-developing/exercises/Topics/Generating random numbers and strings/Simple Password Generator/main.go
+++ courses/path-to-go-developing/exercises/Topics/Generating random numbers and strings/Simple Password Generator/main.go
@@ -6,18 +6,23 @@ import (
 )
 
 func main() {
-	upperCase := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	upperCase := "ABCDEFGHIJKLMOPQRSTUVWXYZ"
 	lowerCase := "abcdefghijklmnopqrstuvwxyz"
 	number := "0123456789"
 	specialSymbol := "!?$&%#"
 
 	var seed int64
-	_, err := fmt.Scanln(&seed)
-	if err != nil {
-		return
-	}
+	fmt.Scanln(&seed)
 	rand.Seed(seed)
 
 	// put your code here
+	l := 4
 
+	password := make([]byte, l)
+	password[0] = upperCase[rand.Intn(len(upperCase))]
+	password[1] = lowerCase[rand.Intn(len(lowerCase))]
+	password[2] = number[rand.Intn(len(number))]
+	password[3] = specialSymbol[rand.Intn(len(specialSymbol))]
+
+	fmt.Println(string(password))
 }
