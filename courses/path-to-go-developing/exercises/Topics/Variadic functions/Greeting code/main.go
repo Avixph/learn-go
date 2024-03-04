3cbdf97cc3d2c71009fad38c3710ae3f60e35346
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2024-03-04 14:07:05 -0500

    worked on varadic function excercises

--- courses/path-to-go-developing/exercises/Topics/Variadic functions/Greeting code/main.go
+++ courses/path-to-go-developing/exercises/Topics/Variadic functions/Greeting code/main.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 func main() {
-    var prefix, name1, name2 string
-    fmt.Scan(&prefix, &name1, &name2)
+	var prefix, name1, name2 string
+	_, err := fmt.Scan(&prefix, &name1, &name2)
+	if err != nil {
+		return
+	}
 
-    for _, line := range Greeting(prefix, name1, name2) {
-        fmt.Println(line)
-    }
+	for _, line := range Greeting(prefix, name1, name2) {
+		fmt.Println(line)
+	}
 }
 
-func Greeting(prefix ?, name ?) []string {
-    // your code goes here
+func Greeting(prefix string, names ...string) []string {
+	var greet []string
+	for _, name := range names {
+		greet = append(greet, fmt.Sprintf("%s %s", prefix, name))
+	}
+	return greet
 }
